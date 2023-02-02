2dfaa1e728b26d3e25d1fe41a8f0a26e6207f444
Peter Milne <milnepe@gmail.com>
2023-02-02 17:04:10 +0000

    Remove framerate
    Framerate is set according to capture view

--- face_detect.py
+++ face_detect.py
@@ -19,13 +19,12 @@ def gstreamer_pipeline(
     capture_height=1080,
     display_width=960,
     display_height=540,
-    framerate=30,
     flip_method=0,
 ):
     return (
         "nvarguscamerasrc ! "
         "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "width=(int)%d, height=(int)%d ! "
         "nvvidconv flip-method=%d ! "
         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
         "videoconvert ! "
@@ -33,7 +32,6 @@ def gstreamer_pipeline(
         % (
             capture_width,
             capture_height,
-            framerate,
             flip_method,
             display_width,
             display_height,
